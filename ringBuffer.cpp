04ffe939c0ac449bd60bae620a60c04d1653a3d2
OHYA Satoshi <44540173+SatoshiRC@users.noreply.github.com>
2024-08-27 14:24:37 +0900

    Create ringBuffer.cpp

---
+++ ringBuffer.cpp
@@ -0,0 +1,40 @@
+#include<array>
+#include<string>
+
+template<typename _Tp, std::size_t _Nm>
+struct MovingAverage: public std::array<_Tp, _Nm>{
+    template<typename T=float>
+
+    void push(_Tp value){
+        this->data()[lastIndex] = value;
+        lastIndex = headIndex;
+        headIndex = (lastIndex+1)%_Nm;
+    }
+
+    void setHeadIndex(uint8_t num){
+        if(num<0){
+            return;
+        }
+        headIndex = num%_Nm;
+        lastIndex = (headIndex - 1) %_Nm;
+    }
+
+    uint8_t *getBufferPtr(){
+        return (uint8_t*)this->data();
+    }
+
+private:
+    uint8_t headIndex = 0;
+    uint8_t lastIndex = _Nm-1;
+};
+
+template<typename T, std::size_t N>
+std::ostream& operator<<(std::ostream& o, MovingAverage<T,N>& obj){
+	for(uint8_t n=0; n<N; n++){
+        o << std::to_string(obj[n]);
+        if(n<N-1){
+            o << ", ";
+        }
+    }
+	return o;
+}
