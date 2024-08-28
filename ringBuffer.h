e96bdeeead53653cd36018da4d3295559377b1c1
OHYA Satoshi <conatus.11099@gmail.com>
2024-08-28 23:51:49 +0900

    update

--- ringBuffer.h
+++ ringBuffer.h
@@ -2,20 +2,17 @@
 #include<string>
 
 template<typename _Tp, std::size_t _Nm>
-struct MovingAverage: public std::array<_Tp, _Nm>{
+struct RingBuffer: public std::array<_Tp, _Nm>{
     template<typename T=float>
 
     void push(_Tp value){
-        this->data()[lastIndex] = value;
+        this->data()[headIndex] = value;
         lastIndex = headIndex;
         headIndex = (lastIndex+1)%_Nm;
     }
 
     void setHeadIndex(uint8_t num){
-        if(num<0){
-            return;
-        }
-        headIndex = num%_Nm;
+        headIndex = (headIndex+num)%_Nm;
         lastIndex = (headIndex - 1) %_Nm;
     }
 
@@ -23,17 +20,20 @@ struct MovingAverage: public std::array<_Tp, _Nm>{
         return (uint8_t*)this->data();
     }
 
+    _Tp& operator[](int8_t __n) noexcept{
+    	return this->data()[(headIndex+__n)%_Nm];
+    }
 private:
     uint8_t headIndex = 0;
     uint8_t lastIndex = _Nm-1;
 };
 
 template<typename T, std::size_t N>
-std::ostream& operator<<(std::ostream& o, MovingAverage<T,N>& obj){
+std::ostream& operator<<(std::ostream& o, RingBuffer<T,N>& obj){
 	for(uint8_t n=0; n<N; n++){
         o << std::to_string(obj[n]);
         if(n<N-1){
-            o << ", ";
+            o << std::string(", ");
         }
     }
 	return o;
